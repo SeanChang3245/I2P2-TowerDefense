1fa222bad5510a40757f380608ebda31bf9fcc91
SeanChang-3245 <70448313+SeanChang-3245@users.noreply.github.com>
2024-06-02 00:02:49 +0800

    Only test
    no modification to package

--- Enemy/AdvancedTankEnemy.cpp
+++ Enemy/AdvancedTankEnemy.cpp
@@ -2,8 +2,8 @@
 
 #include "AdvancedTankEnemy.hpp"
 
-AdvancedTankEnemy::AdvancedTankEnemy(int x, int y) : 
-    Enemy("play/enemy-4.png", x, y, 20, 15, 200, 75) 
+AdvancedTankEnemy::AdvancedTankEnemy(int x, int y) :
+    Enemy("play/enemy-4.png", x, y, 20, 15, 200,  75) 
 {
 	// TODO: [CUSTOM-TOOL] You can imitate the 2 files: 'SoldierEnemy.hpp', 'SoldierEnemy.cpp' to create a new enemy.
 	kill_score = 300;
